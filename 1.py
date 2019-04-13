7c386501acfdd111c1f334f76e18ceeb427c9d05
Yazilimca <info@bizimbilisim.com>
2019-04-13 03:36:28 +0300

    python

---
+++ 1.py
@@ -0,0 +1,34 @@
+﻿while True:
+    secenek1 = "(1) toplama"
+    secenek2 = "(2) cikarma"
+    secenek3 = "(3) carpma"
+    secenek4 = "(4) bolme"
+
+    print "Yapmak istediginiz islemin numarasini giriniz"
+
+    print secenek1
+    print secenek2
+    print secenek3
+    print secenek4
+
+    soru = raw_input(":")
+
+    if soru == "1":
+        sayi1 = input("Ilk sayiyi girin")
+        sayi2 = input("Ikinci sayiyi girin")
+        print sayi1, "+", sayi2, "=", sayi1 + sayi2
+
+    if soru == "2":
+        sayi3 = input("Ilk sayiyi girin")
+        sayi4 = input("Ikinci sayiyi girin")
+        print sayi3, "-", sayi4, "=", sayi3 - sayi4
+
+    if soru == "3":
+        sayi5 = input("Ilk sayiyi girin")
+        sayi6 = input("Ikinci sayiyi girin")
+        print sayi5, "X", sayi6, "=", sayi5 * sayi6
+
+    if soru == "4":
+        sayi7 = input("Ilk sayiyi girin")
+        sayi8 = input("Ikinci sayiyi girin")
+        print sayi7, "/", sayi8, "=", sayi7 / sayi8
---
+++ 2.py
@@ -0,0 +1,33 @@
+﻿import random
+
+
+liste = ''
+parola = ''
+
+cevap_sayi = input('Parolanın içerisinde sayı olsun mu? [E/H]: ')
+if cevap_sayi == 'E':
+  liste = liste + '0123456789'
+
+cevap_harf = input('Parolanın içerisinde harf olsun mu? [E/H]: ')
+if cevap_harf == 'E':
+  liste = liste + 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+cevap_karakter = input('Parolanın içerisinde özel karakter olsun mu? [E/H]: ')
+if cevap_karakter == 'E':
+  liste = liste + '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+
+minimum = int(input('Parola minimum karakter olsun?: '))
+maksimum = int(input('Parola maksimum kaç karakter olsun?: '))
+
+uzunluk = random.randint(minimum, maksimum)
+
+if cevap_sayi != 'E' and cevap_harf != 'E' and cevap_karakter != 'E':
+  print('En az bir cevap E olmalıdır\n')
+else:
+  for c in range(uzunluk):
+    parola += random.choice(liste)
+
+
+    
+print(parola)
+
